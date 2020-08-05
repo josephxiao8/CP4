1e50e399fcd3aa43ca3ee70e9fc58a5bd64cac0a
josephxiao8 <55932881+josephxiao8@users.noreply.github.com>
2020-08-04 23:55:48 -0400

    Create 11547.cpp

---
+++ Chapter 1/Getting Started: The Easy Problems/11547.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ldb;
+typedef unsigned int ui;
+typedef unsigned long ul;
+ 
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+ 
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+ 
+typedef priority_queue<pi> pq;
+typedef priority_queue<int,vector<int>,greater<int>> pqg;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+ 
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+ll t;
+cin>>t;
+while(t--){
+	ll n;
+	cin>>n;
+	ll temp=(((((((n*567)/9)+7492)*235)/47)-498)/10)%10;
+	cout<<(temp>0 ? temp:-temp)<<endl;
+}
+return 0; 
+}
