14f07549ab548cfcee97319676e43e1279689538
josephxiao8 <55932881+josephxiao8@users.noreply.github.com>
2020-08-23 11:55:44 -0400

    Create shopaholic.cpp

---
+++ Chapter 3/Greedy/shopaholic.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ldb;
+typedef unsigned int ui;
+typedef unsigned long ul;
+ 
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+ 
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+ 
+typedef priority_queue<tuple<int,int,int> > pq;
+typedef priority_queue<int,vector<int>,greater<int>> pqg;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+ 
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+ll n;
+cin>>n;
+
+vl v(n);
+for(ll i=0; i<n; i++) cin>>v[i];
+sort(v.rbegin(), v.rend());
+
+ll ans=0;
+for(ll i=0; i<n; i+=3){
+    if(i+2<n) ans+=v[i+2];
+}
+cout<<ans<<endl;
+
+return 0;
+}
