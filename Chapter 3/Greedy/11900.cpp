e5eed4da030f7ec7a757366d48914c51fa73443d
josephxiao8 <55932881+josephxiao8@users.noreply.github.com>
2020-08-22 22:07:36 -0400

    Create 11900.cpp

---
+++ Chapter 3/Greedy/11900.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ldb;
+typedef unsigned int ui;
+typedef unsigned long ul;
+ 
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+ 
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+ 
+typedef priority_queue<tuple<int,int,int> > pq;
+typedef priority_queue<int,vector<int>,greater<int>> pqg;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+ 
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+
+int main() {
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+int t, casenum=1;
+cin>>t;
+
+while(t--){
+	int n,p,q;
+	cin>>n>>p>>q;
+	vi v(n);
+	for(int i=0; i<n; i++) cin>>v[i];
+	sort(v.begin(),v.end());
+	
+	int ans=0;
+	int weight=0;
+	for(int i=0; i<n; i++){
+		if(ans+1>p || weight+v[i]>q) break;
+		ans++;
+		weight+=v[i];
+	}
+	cout<<"Case "<<casenum++<<": "<<ans<<endl;
+}
+
+return 0;
+}
